a0410bc5090a346bce8e45a082def9a9910df973
discoshrimp <roydega@gmail.com>
2018-06-24 15:43:29 -0700

    working code, could use tweaking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,55 @@
-var count=20;
-
+var count=30;
+var checkOff ={
+"correct": 0,
+"incorrect": 0,
+}
 var counter= setInterval(timer, 1000);
 function reset(){
-    count=20;
+    count=30;
     counter= setInterval(timer, 1000);
     timer();
 }
 function timer(){
     count=count-1;
 
-    if(count<=0){
-        clearInterval(counter);
+    if(count<1){
+        alert('Times up!');
         reset()
     }
-    console.log(count);
-    $("#timer").text("Time left: " + count + " seconds")
+
+    $('#timer').text('Time left: ' + count + ' seconds')
 }
+
+timer();
+$(document).ready(function(){
+$('#done').on("click", function(){
+    console.log("submitted");
+    var answerSheet =[];
+    var answer;
+    var isChecked ={
+        "one":$("#1B").is(":checked"),
+        "two":$("#2C").is(":checked"),
+        "three":$("#3A").is(":checked"),
+        "four":$("#4C").is(":checked"),
+        "five":$("#5C").is(":checked"),
+    }
+    console.log(isChecked);
+
+answerSheet.push(isChecked.one);
+answerSheet.push(isChecked.two);
+answerSheet.push(isChecked.three);
+answerSheet.push(isChecked.four);
+answerSheet.push(isChecked.five);
+console.log(answerSheet);
+for(var i=0; i<answerSheet.length; i++){
+    answer = answerSheet[i]
+    console.log(answer)
+    if(answer === true){
+        checkOff.correct++;
+    } else if(answer != true) {
+        checkOff.incorrect++;
+    }
+}console.log(checkOff)
+$("#spacer").append("<p class='answer-display'> Correct: " + checkOff.correct +"<br> Incorrect: "+ checkOff.incorrect + "</p>")
+})
+})
